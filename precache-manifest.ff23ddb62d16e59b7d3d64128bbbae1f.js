70a630e8cb047683252306456393c68ce8bd938b
shubhamjainkcity <62244970+shubhamjainkcity@users.noreply.github.com>
2020-06-24 12:26:08 +0530

    Updates

---
+++ precache-manifest.ff23ddb62d16e59b7d3d64128bbbae1f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10687496f40cdda81375a260df35885e",
+    "url": "/shubham-portfolio/index.html"
+  },
+  {
+    "revision": "bd1ecfa3d689e5994033",
+    "url": "/shubham-portfolio/static/css/2.b59c3120.chunk.css"
+  },
+  {
+    "revision": "92ceb8b8289188ea9325",
+    "url": "/shubham-portfolio/static/css/main.5a823fc1.chunk.css"
+  },
+  {
+    "revision": "bd1ecfa3d689e5994033",
+    "url": "/shubham-portfolio/static/js/2.7367bb68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shubham-portfolio/static/js/2.7367bb68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ceb8b8289188ea9325",
+    "url": "/shubham-portfolio/static/js/main.68f16bda.chunk.js"
+  },
+  {
+    "revision": "43fac7d0c6566763d6dc",
+    "url": "/shubham-portfolio/static/js/runtime-main.457bf592.js"
+  },
+  {
+    "revision": "710f053e293bb7bcd4861edc00e1782d",
+    "url": "/shubham-portfolio/static/media/+91 96948 19545 20200118_210800.710f053e.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shubham-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shubham-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shubham-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shubham-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shubham-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "96e0ea172418b9d7999b007814807622",
+    "url": "/shubham-portfolio/static/media/img-01.96e0ea17.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shubham-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "40418cc8089c085a8964a7b3b20af941",
+    "url": "/shubham-portfolio/static/media/searchpng.com-drdo-logo-png-image-free-download.40418cc8.png"
+  }
+]);
